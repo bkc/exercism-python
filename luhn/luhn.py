d1f97d112caf24ac80ad24552b6856d2cc9c2851
Brad Clements <bkc@murkworks.com>
2022-02-12 16:53:59 -0500

    added luhn solution

--- luhn/luhn.py
+++ luhn/luhn.py
@@ -1,6 +1,24 @@
 class Luhn:
     def __init__(self, card_num):
-        pass
+        self.card_number = card_num.replace(" ", "")
 
     def valid(self):
-        pass
+        if not self.card_number.isdigit() or len(self.card_number) <= 1:
+            return False
+
+        # personally I find this less readable than using a regular for loop and
+        # incrementing a total value.  However it does demonstrate nested if expressions
+        # fairly well
+        return (
+            not sum(
+                integer_digit
+                if offset % 2 == 0
+                else (integer_digit * 2) - 9
+                if integer_digit >= 5
+                else integer_digit * 2
+                for offset, integer_digit in list(
+                    enumerate(int(_) for _ in self.card_number[::-1])
+                )
+            )
+            % 10
+        )
